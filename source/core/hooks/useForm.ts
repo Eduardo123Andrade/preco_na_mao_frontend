5700d21a75248eeef38d07c452a9bc678e945d9a
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-17 22:02:10 -0300

    Fix - corrigida a tipagem do useform

--- source/core/hooks/useForm.ts
+++ source/core/hooks/useForm.ts
@@ -1,48 +1,52 @@
-import { useFormik, FormikConfig, FormikValues, FieldInputProps as FieldInputPropsFormik } from "formik"
+import { useFormik, FormikConfig, FormikValues, FieldInputProps as FieldInputPropsFormik, FormikHelpers } from "formik"
 import { useCallback, useImperativeHandle, useMemo, useRef } from "react"
 
 
-interface UseFormProps extends FormikConfig<FormikValues> { }
-
-export const useForm = <FieldInputProps = unknown>(props: UseFormProps) => {
-
-
-  const formik = useFormik({
-    validateOnMount: true,
-    validateOnChange: true,
-    ...props,
-  })
-
-  useImperativeHandle(null, () => formik, [formik])
-
-  const { getFieldProps: _getFieldProps, getFieldMeta } = formik
-
-  const inputEvents = useRef<any>({})
-
-
-  const getFieldProps = useCallback(
-    (key: string) => {
-      const { onChange, value, onBlur: _onBlur } = _getFieldProps(key) as FieldInputPropsFormik<string>
-      const { error, touched } = getFieldMeta(key)
-
-      // const status = getCurrentStatus(touched, error, value)
-
-      if (!inputEvents.current[key]) {
-        inputEvents.current[key] = {
-          onChange: onChange(key),
-          onBlur: _onBlur(key),
-        }
-      }
-
-      const { onBlur, onChange: onChangeText } = inputEvents.current[key]
-      const subtitle = touched && error ? error : null
-
-      // return {status, onChangeText, onBlur, value, subtitle}
-      return { onChangeText, onBlur, value, subtitle }
-    },
-    [_getFieldProps, getFieldMeta],
-  )
-  // return formik
-  return useMemo(() => ({ ...formik, getFieldProps }), [formik, getFieldProps])
-
+interface UseFormProps<Value> {
+  validationSchema: object
+  initialValues: Value
+  onSubmit: (values?: Value, helper?: FormikHelpers<Value>) => void
 }
+
+export const useForm =
+  <Value = unknown, FieldInputProps = unknown>(props: UseFormProps<Value>) => {
+
+    const formik = useFormik({
+      validateOnMount: true,
+      validateOnChange: true,
+      ...props,
+    })
+
+    useImperativeHandle(null, () => formik, [formik])
+
+    const { getFieldProps: _getFieldProps, getFieldMeta } = formik
+
+    const inputEvents = useRef<any>({})
+
+
+    const getFieldProps = useCallback(
+      (key: string) => {
+        const { onChange, value, onBlur: _onBlur } = _getFieldProps(key) as FieldInputPropsFormik<FieldInputProps>
+        const { error, touched } = getFieldMeta(key)
+
+        // const status = getCurrentStatus(touched, error, value)
+
+        if (!inputEvents.current[key]) {
+          inputEvents.current[key] = {
+            onChange: onChange(key),
+            onBlur: _onBlur(key),
+          }
+        }
+
+        const { onBlur, onChange: onChangeText } = inputEvents.current[key]
+        const subtitle = touched && error ? error : null
+
+        // return {status, onChangeText, onBlur, value, subtitle}
+        return { onChangeText, onBlur, value, subtitle }
+      },
+      [_getFieldProps, getFieldMeta],
+    )
+    // return formik
+    return useMemo(() => ({ ...formik, getFieldProps }), [formik, getFieldProps])
+
+  }
