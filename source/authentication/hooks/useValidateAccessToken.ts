dda181cd1acf3d151d2f80f2a4980c9599b54602
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-17 23:37:58 -0300

    Feat - Criado hook para validar o token de acesso

---
+++ source/authentication/hooks/useValidateAccessToken.ts
@@ -0,0 +1,34 @@
+import { usePostRequest } from "core/hooks"
+import { UsePostRequestOptionsType } from "core/hooks/usePostRequest"
+
+
+interface RequestValidateAccessTokenVariable {
+  cpf: string,
+  accessToken: string
+}
+
+interface RequestValidateAccessTokenState {
+  isLoading: boolean
+}
+
+interface RequestValidateAccessTokenActions {
+  requestValidateAccessToken: (cpf: string, accessToken: string) => void
+}
+
+type RequestValidateAccessTokenData = [
+  state: RequestValidateAccessTokenState,
+  actions: RequestValidateAccessTokenActions
+]
+
+
+export const useValidateAccessToken = (options: UsePostRequestOptionsType<unknown, unknown, RequestValidateAccessTokenVariable>): RequestValidateAccessTokenData => {
+
+  const { mutate, isLoading } = usePostRequest('/validate-access-token', options)
+
+  const requestValidateAccessToken = (cpf: string, accessToken: string) => {
+    mutate({ cpf, accessToken })
+
+  }
+
+  return [{ isLoading }, { requestValidateAccessToken }]
+}
