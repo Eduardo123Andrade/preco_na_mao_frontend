d7880b4b12b8764da99779889e02285e6fe96a1a
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-19 10:20:35 -0300

    Feat - criado hook para centralizar a logica de exibição do modal

--- source/authentication/hooks/index.ts
+++ source/authentication/hooks/index.ts
@@ -1,3 +1,4 @@
+export { useErrorModal } from './useErrorModal'
 export { useForgottenPassword } from './useForgottenPassword'
 export { useLogin } from './useLogin'
 export { useRequestAccessToken } from './useRequestAccessToken'
@@ -5,5 +6,4 @@ export { useRequestSingUp } from './useRequestSingUp'
 export { useSingUp } from './useSingUp'
 export { useUpdatePassword } from './useUpdatePassword'
 export { useValidateAccessToken } from './useValidateAccessToken'
-
 
