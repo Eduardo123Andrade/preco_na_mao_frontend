4b7949f09db42d90176e8141fc78494efadfa1ff
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-25 20:37:14 -0300

    Fix - fixed name

--- source/shopping-list/utils/mockedData.ts
+++ source/shopping-list/utils/mockedData.ts
@@ -3,22 +3,22 @@ import { Item, ShoppingList } from 'shopping-list/interfaces';
 export const MOCKED_SHOPPING_LIST_ITEM: Item[] = [
   {
     id: '1',
-    name: 'Item 1',
+    name: 'Produto 1',
     price: 100
   },
   {
     id: '2',
-    name: 'Item 2',
+    name: 'Produto 2',
     price: 150
   },
   {
     id: '3',
-    name: 'Item 3',
+    name: 'Produto 3',
     price: 50
   },
   {
     id: '4',
-    name: 'Item 4',
+    name: 'Produto 4',
     price: 200
   },
 ]
@@ -29,30 +29,30 @@ export const MOCKED_SHOPPING_LIST: ShoppingList[] = [
     id: '1',
     date: new Date(),
     items: MOCKED_SHOPPING_LIST_ITEM,
-    name: "Produto 1"
+    name: "Lista 1"
   },
   {
     id: '2',
     date: new Date(),
     items: MOCKED_SHOPPING_LIST_ITEM,
-    name: "Produto 2"
+    name: "Lista 2"
   },
   {
     id: '3',
     date: new Date(),
     items: MOCKED_SHOPPING_LIST_ITEM,
-    name: "Produto 3"
+    name: "Lista 3"
   },
   {
     id: '4',
     date: new Date(),
     items: MOCKED_SHOPPING_LIST_ITEM,
-    name: "Produto 4"
+    name: "Lista 4"
   },
   {
     id: '5',
     date: new Date(),
     items: MOCKED_SHOPPING_LIST_ITEM,
-    name: "Produto 5"
+    name: "Lista 5"
   },
 ]
