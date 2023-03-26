98c188b8795ae76491e0ab9fe20c0ba9ff031502
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-26 11:28:40 -0300

    Fix - fixed icon component

--- source/core/components/Icon.tsx
+++ source/core/components/Icon.tsx
@@ -1,1 +1,19 @@
-export { default as Icon } from 'react-native-vector-icons/MaterialIcons'
+import React from 'react'
+
+import { default as MaterialIcons } from 'react-native-vector-icons/MaterialIcons'
+import { default as AntDesign } from 'react-native-vector-icons/AntDesign'
+import { IconProps as RNVIProps } from 'react-native-vector-icons/Icon'
+
+
+type IconMode = 'material_icons' | 'ant_design'
+interface IconProps extends RNVIProps {
+  mode?: IconMode
+  // name: string
+}
+
+export const Icon: React.FC<IconProps> = ({ mode = 'material_icons', ...rest }) => {
+  if (mode === 'ant_design')
+    return <AntDesign {...rest} />
+
+  return <MaterialIcons {...rest} />
+}
