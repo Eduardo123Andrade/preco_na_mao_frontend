91cf3e6935408597e7036b6585248381724d5d90
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-26 12:04:45 -0300

    Feat - finished shopping list details screen layout

--- source/shopping-list/screens/ShoppingListDetailsScreen.tsx
+++ source/shopping-list/screens/ShoppingListDetailsScreen.tsx
@@ -1,9 +1,11 @@
 import { StackNavigationOptions } from '@react-navigation/stack'
 import { Icon, Screen, Text } from 'core/components'
+import { formatPrice } from 'core/utils'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 import { Product } from 'shopping-list/components'
-import { ShoppingList } from 'shopping-list/interfaces'
+import { Product as ProductInterface, ShoppingList } from 'shopping-list/interfaces'
 import { MOCKED_SHOPPING_LIST } from 'shopping-list/utils'
 
 interface ShoppingListDetailsScreenProps {
@@ -13,22 +15,58 @@ interface ShoppingListDetailsScreenProps {
 
 const [currentShoppingList] = MOCKED_SHOPPING_LIST
 
-export const ShoppingListDetailsScreen = (props: ShoppingListDetailsScreenProps) => {
-  return (
-    <Screen style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text>
-          {currentShoppingList.name}
-        </Text>
-        <Icon
-          name='play-arrow'
-          size={20}
-          color='#AAA'
-        />
-      </View>
-      <Product />
-      <View>
+interface RenderItemProps {
+  item: ProductInterface
+}
 
+const renderItem = ({ item }: RenderItemProps) => {
+  return (
+    <View style={styles.productContainer} key={`${item.id}`}>
+      <Product product={item} />
+    </View>
+  )
+}
+
+const sumValues = (previous: number, current: ProductInterface) =>
+  previous + (current.quantity * current.price)
+
+export const ShoppingListDetailsScreen = (props: ShoppingListDetailsScreenProps) => {
+  const { items } = currentShoppingList
+  const totalPrice = items.reduce(sumValues, 0)
+  const formattedPrice = formatPrice(totalPrice)
+
+  return (
+    <Screen contentContainerStyles={styles.container}>
+      <View>
+        <View style={styles.headerContainer}>
+          <Text>
+            {currentShoppingList.name}
+          </Text>
+          <View style={styles.iconContainer}>
+            <Icon
+              name='play-arrow'
+            />
+            <Icon
+              name='edit'
+            />
+          </View>
+        </View>
+
+        <View style={styles.listContainer}>
+          <FlatList
+            data={items}
+            renderItem={renderItem}
+          />
+        </View>
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Text fontSize={15}>
+          Total
+        </Text>
+        <Text>
+          {`R$ ${formattedPrice}`}
+        </Text>
       </View>
     </Screen>
   )
@@ -45,12 +83,32 @@ ShoppingListDetailsScreen.NavigationOptions = navigationOptions
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'space-between'
   },
   headerContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    // backgroundColor: '#F11',
     paddingVertical: 20,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 0.2
+  },
+  listContainer: {
+    marginHorizontal: -15,
+  },
+  productContainer: {
+    paddingTop: 5
+  },
+  footerContainer: {
+    backgroundColor: '#AAA',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+    marginHorizontal: -20,
   }
 })
