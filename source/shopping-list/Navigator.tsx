62428f6f72bcf6f529dfacda9a7924b60c9e363c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-25 21:07:37 -0300

    Feat - using provider

--- source/shopping-list/Navigator.tsx
+++ source/shopping-list/Navigator.tsx
@@ -3,19 +3,22 @@ import React from 'react'
 import { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs'
 import { ShoppingListHomeScreen } from './screens'
 import { TabBarIcon } from 'core/components'
+import { ShoppingListProvider } from './providers'
 
 
 const { Navigator, Screen } = createStackNavigator()
 
 export const ShoppingListNavigator = () => {
   return (
-    <Navigator>
-      <Screen
-        name='ShoppingListHomeScreen'
-        component={ShoppingListHomeScreen}
-        options={ShoppingListHomeScreen.NavigationOptions}
-      />
-    </Navigator>
+    <ShoppingListProvider>
+      <Navigator>
+        <Screen
+          name='ShoppingListHomeScreen'
+          component={ShoppingListHomeScreen}
+          options={ShoppingListHomeScreen.NavigationOptions}
+        />
+      </Navigator>
+    </ShoppingListProvider>
   )
 }
 
