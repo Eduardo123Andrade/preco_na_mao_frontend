4de7c417b58cf4194487ae6b6cb22ccd2ea6663e
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-30 20:42:01 -0300

    Fix - ajuste mocked data

--- source/shopping-list/utils/mockedData.ts
+++ source/shopping-list/utils/mockedData.ts
@@ -3,25 +3,25 @@ import { Product, ShoppingList } from 'core/interfaces'
 
 export const MOCKED_SHOPPING_LIST_ITEM: Product[] = [
   {
-    id: '1',
+    id: '25',
     name: 'Produto 1',
     price: 100,
     quantity: 1,
   },
   {
-    id: '2',
+    id: '26',
     name: 'Produto 2',
     price: 150,
     quantity: 2,
   },
   {
-    id: '3',
+    id: '27',
     name: 'Produto 3',
     price: 50,
     quantity: 3,
   },
   {
-    id: '4',
+    id: '28',
     name: 'Produto 4',
     price: 200,
     quantity: 4,
@@ -65,32 +65,182 @@ export const MOCKED_MARKETPLACE_LIST: Marketplace[] = [
   {
     id: '1',
     name: "Mercado 1",
-    products: MOCKED_SHOPPING_LIST_ITEM
+    products: [
+      {
+        id: '1',
+        name: 'P1 - M1',
+        price: 50,
+        quantity: 10,
+      },
+      {
+        id: '2',
+        name: 'P2 - M1',
+        price: 75,
+        quantity: 10
+      },
+      {
+        id: '3',
+        name: 'P3- M1',
+        price: 100,
+        quantity: 10,
+      },
+      {
+        id: '4',
+        name: 'P4 - M1',
+        price: 30,
+        quantity: 1
+      }
+    ]
   },
   {
     id: '2',
     name: "Mercado 2",
-    products: MOCKED_SHOPPING_LIST_ITEM
+    products: [
+      {
+        id: '5',
+        name: 'P1 - M2',
+        price: 50,
+        quantity: 10,
+      },
+      {
+        id: '6',
+        name: 'P2 - M2',
+        price: 75,
+        quantity: 10
+      },
+      {
+        id: '7',
+        name: 'P3- M2',
+        price: 100,
+        quantity: 10,
+      },
+      {
+        id: '8',
+        name: 'P4 - M2',
+        price: 30,
+        quantity: 1
+      }
+    ]
   },
   {
     id: '3',
     name: "Mercado 3",
-    products: MOCKED_SHOPPING_LIST_ITEM
+    products: [
+      {
+        id: '9',
+        name: 'P1 - M3',
+        price: 50,
+        quantity: 10,
+      },
+      {
+        id: '10',
+        name: 'P2 - M3',
+        price: 75,
+        quantity: 10
+      },
+      {
+        id: '11',
+        name: 'P3- M3',
+        price: 100,
+        quantity: 10,
+      },
+      {
+        id: '12',
+        name: 'P4 - M3',
+        price: 30,
+        quantity: 1
+      }
+    ]
   },
   {
     id: '4',
     name: "Mercado 4",
-    products: MOCKED_SHOPPING_LIST_ITEM
+    products: [
+      {
+        id: '13',
+        name: 'P1 - M4',
+        price: 50,
+        quantity: 10,
+      },
+      {
+        id: '14',
+        name: 'P2 - M4',
+        price: 75,
+        quantity: 10
+      },
+      {
+        id: '15',
+        name: 'P3- M4',
+        price: 100,
+        quantity: 10,
+      },
+      {
+        id: '16',
+        name: 'P4 - M4',
+        price: 30,
+        quantity: 1
+      }
+    ]
   },
   {
     id: '5',
     name: "Mercado 5",
-    products: MOCKED_SHOPPING_LIST_ITEM
+    products: [
+      {
+        id: '17',
+        name: 'P1 - M5',
+        price: 50,
+        quantity: 10,
+      },
+      {
+        id: '18',
+        name: 'P2 - M5',
+        price: 75,
+        quantity: 10
+      },
+      {
+        id: '19',
+        name: 'P3- M5',
+        price: 100,
+        quantity: 10,
+      },
+      {
+        id: '20',
+        name: 'P4 - M5',
+        price: 30,
+        quantity: 1
+      }
+    ]
   },
   {
     id: '6',
     name: "Mercado 6",
-    products: MOCKED_SHOPPING_LIST_ITEM
+    products: [
+      {
+        id: '21',
+        name: 'P1 - M6',
+        price: 50,
+        quantity: 10,
+      },
+      {
+        id: '22',
+        name: 'P2 - M6',
+        price: 75,
+        quantity: 10
+      },
+      {
+        id: '23',
+        name: 'P3- M6',
+        price: 100,
+        quantity: 10,
+      },
+      {
+        id: '24',
+        name: 'P4 - M6',
+        price: 30,
+        quantity: 1
+      }
+    ]
   },
 ]
 
