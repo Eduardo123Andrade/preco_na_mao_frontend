e869653c8c2cd88abb7022e0b277d78446c03018
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-29 21:10:36 -0300

    Fix - fixed formatDate

--- source/core/utils/formatDate.ts
+++ source/core/utils/formatDate.ts
@@ -1,9 +1,12 @@
 import { zeroToLeft } from "./zeroToLeft"
 
 export const formatDate = (date: Date) => {
-  const day = zeroToLeft(date.getDate())
-  const month = zeroToLeft(date.getMonth() + 1)
-  const year = date.getFullYear()
+  const currentDate = new Date(date)
+  // console.log({ date }, 'oi')
+  // if (!date) return ''
+  const day = zeroToLeft(currentDate.getDate())
+  const month = zeroToLeft(currentDate.getMonth() + 1)
+  const year = currentDate.getFullYear()
 
   return `${day}/${month}/${year}`
 }
