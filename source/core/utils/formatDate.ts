0082098ce497943732b15ded384ea0b2823216b8
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-03-29 21:36:43 -0300

    Fix - removed commented lines

--- source/core/utils/formatDate.ts
+++ source/core/utils/formatDate.ts
@@ -2,8 +2,6 @@ import { zeroToLeft } from "./zeroToLeft"
 
 export const formatDate = (date: Date) => {
   const currentDate = new Date(date)
-  // console.log({ date }, 'oi')
-  // if (!date) return ''
   const day = zeroToLeft(currentDate.getDate())
   const month = zeroToLeft(currentDate.getMonth() + 1)
   const year = currentDate.getFullYear()
