205f24566bf87d23dbb005d0ccc1b55d2c03f8c4
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-04-01 20:31:05 -0300

    Fix - Added space on icon

--- source/core/components/Icon.tsx
+++ source/core/components/Icon.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import { default as MaterialIcons } from 'react-native-vector-icons/MaterialIcons'
 import { default as AntDesign } from 'react-native-vector-icons/AntDesign'
 import { IconProps as RNVIProps } from 'react-native-vector-icons/Icon'
+import { StyleSheet } from 'react-native'
 
 
 type IconMode = 'material_icons' | 'ant_design'
 interface IconProps extends RNVIProps {
   mode?: IconMode
-  // name: string
 }
 
 export const Icon: React.FC<IconProps> = ({
@@ -16,10 +16,17 @@ export const Icon: React.FC<IconProps> = ({
   color = '#000',
   size = 20,
   ...rest }) => {
-  const props = { color, size, ...rest }
+  const style = styles.container
+  const props = { color, size, style, ...rest }
 
   if (mode === 'ant_design')
-    return <AntDesign {...props} />
+    return <AntDesign style={styles.container} {...props} />
 
-  return <MaterialIcons {...props} />
+  return <MaterialIcons style={styles.container} {...props} />
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5
+  }
+})
