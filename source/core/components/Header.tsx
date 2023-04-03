ab7d10ec5e9a26f5eb8e8d31a9c0ce9267fdca5c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-04-03 19:48:32 -0300

    Feat - created header menu

---
+++ source/core/components/Header.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Icon } from './Icon'
+import { DrawerNavigation } from './drawer-navigator'
+
+interface HeaderProps { }
+
+export const Header: React.FC<HeaderProps> = () => {
+  const [visible, setVisible] = useState(false)
+
+  const onPress = () => {
+    setVisible(true)
+
+  }
+
+  const onRequestClose = () => {
+    setVisible(false)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Icon onPress={onPress} name='menu' size={24} />
+      <DrawerNavigation visible={visible} onRequestClose={onRequestClose} />
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 5,
+  }
+})
---
+++ source/core/components/drawer-navigator/DrawerNavigation.tsx
@@ -0,0 +1,109 @@
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Modal, StyleSheet, View } from 'react-native'
+import { Text } from '../Text'
+import { Touchable } from '../Touchable'
+import { DrawerNavigatorItem } from './DrawerNavigatorItem'
+
+
+const BORDER_RADIUS = 20
+
+interface DrawerNavigationProps {
+  visible: boolean
+  onRequestClose: () => void
+}
+
+export const DrawerNavigation: React.FC<DrawerNavigationProps> = ({ visible, onRequestClose }) => {
+  const navigation = useNavigation()
+
+  const onPress = () => {
+    onRequestClose()
+  }
+
+  const logout = () => {
+    console.log('sair')
+  }
+
+  const goToProfile = () => {
+    navigation.navigate('Profile')
+    onRequestClose()
+  }
+
+  const _onRequestClose = () => {
+    onRequestClose()
+  }
+
+  return (
+    <Modal
+      visible={visible}
+      onRequestClose={_onRequestClose}
+      transparent={true}
+      statusBarTranslucent={false}
+      animationType='none'
+    >
+      <View style={styles.container}>
+        <Touchable
+          enableFeedback={false}
+          onPress={onPress}
+          style={styles.subContainer}
+        >
+          <View style={styles.card}>
+            <View>
+              <DrawerNavigatorItem onPress={goToProfile} />
+            </View>
+
+            <View style={styles.footerContainer}>
+              <Text
+                onPress={logout}
+                fontSize={16}
+                color='#0000FF'
+                style={styles.exitText}
+              >
+                Sair
+              </Text>
+
+              <View style={styles.separator} />
+
+              <Text fontSize={12} color='#696969'>
+                Version 1.0.0
+              </Text>
+            </View>
+
+          </View>
+        </Touchable>
+      </View>
+    </Modal>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  subContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: "#000000AA",
+  },
+  card: {
+    width: '60%',
+    height: '100%',
+    backgroundColor: '#FFF',
+    borderBottomRightRadius: BORDER_RADIUS,
+    borderTopRightRadius: BORDER_RADIUS,
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+    justifyContent: 'space-between'
+  },
+  footerContainer: {
+    alignItems: 'flex-end'
+  },
+  exitText: {
+    textDecorationLine: 'underline'
+  },
+  separator: {
+    height: 10
+  }
+})
