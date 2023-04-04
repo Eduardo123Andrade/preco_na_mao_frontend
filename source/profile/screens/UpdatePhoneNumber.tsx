fa87745af0d402a0582c4f7c2bc02d059051ab8c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-04-04 20:15:23 -0300

    Feat - Created update phone screen

--- source/profile/screens/UpdatePhoneNumber.tsx
+++ source/profile/screens/UpdatePhoneNumber.tsx
@@ -1,16 +1,45 @@
 import { StackNavigationOptions } from '@react-navigation/stack'
-import { Text } from 'core/components'
+import { Button, InputText, Screen, Text } from 'core/components'
+import { usePhoneNumberForm } from 'core/hooks'
+import { UserPhoneForm } from 'core/interfaces'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
-
 export const UpdatePhoneNumber = () => {
+  const onSubmit = ({ phone }: UserPhoneForm) => {
+    console.log({ phone })
+  }
+
+  const [{ isValid, fieldProps }, { handleSubmit }] = usePhoneNumberForm({ onSubmit })
+
+  const onPress = () => handleSubmit()
+
   return (
-    <View style={styles.container}>
-      <Text>
-        Olá mundo!
-      </Text>
-    </View>
+    <Screen contentContainerStyles={styles.container}>
+      <View>
+        <View style={styles.titleContainer}>
+          <Text>
+            Preço na Mão
+          </Text>
+        </View>
+
+        <View style={styles.inputTextContainer}>
+          <InputText
+            mask='phone'
+            keyboardType='numeric'
+            placeholder='Celular'
+            {...fieldProps}
+          />
+        </View>
+      </View>
+
+      <Button
+        onPress={onPress}
+        disabled={!isValid}
+      >
+        Avançar
+      </Button>
+    </Screen>
   )
 }
 
@@ -23,6 +52,14 @@ UpdatePhoneNumber.NavigationOptions = navigationOptions
 
 const styles = StyleSheet.create({
   container: {
-
-  }
+    flex: 1,
+    paddingVertical: 20,
+    justifyContent: 'space-between'
+  },
+  titleContainer: {
+    alignItems: 'center',
+  },
+  inputTextContainer: {
+    paddingVertical: 10
+  },
 })
