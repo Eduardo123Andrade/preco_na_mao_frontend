b675239bd61c4e73b7bd8038ef7060bae32aee4b
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-04-11 17:55:49 -0300

    Feat - created default screen options

---
+++ source/core/utils/defaultScreenOptions.ts
@@ -0,0 +1,9 @@
+import { StackNavigationOptions } from "@react-navigation/stack";
+
+export const defaultScreenOptions: StackNavigationOptions = {
+  headerTitleAlign: 'center',
+  headerTintColor: '#228B22',
+  headerStyle: {
+    backgroundColor: '#E2FBED'
+  }
+}
