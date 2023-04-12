be1f21640849f3e60c20794384b95a69c7d643d8
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-04-12 13:53:54 -0300

    Fix - removed commented lines

--- metro.config.js
+++ metro.config.js
@@ -21,20 +21,3 @@ module.exports = (async function () {
     },
   }
 });
-
-// const { getDefaultConfig } = require("metro-config");
-
-// module.exports = (async () => {
-//   const {
-//     resolver: { sourceExts, assetExts }
-//   } = await getDefaultConfig();
-//   return {
-//     transformer: {
-//       babelTransformerPath: require.resolve("react-native-svg-transformer")
-//     },
-//     resolver: {
-//       assetExts: assetExts.filter(ext => ext !== "svg"),
-//       sourceExts: [...sourceExts, "svg"]
-//     }
-//   };
-// })();
