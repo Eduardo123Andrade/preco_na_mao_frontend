e83189dc841f77acfca75939c6f100e17df5cae9
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-04-30 14:54:16 -0300

    Fix - reorder files

---
+++ source/core/interfaces/form/index.ts
@@ -0,0 +1,4 @@
+export { CodeValidationForm } from './codeValidationFormInterface'
+export { UserCpfForm } from "./userCpfFormInterface"
+export { UserPhoneForm } from './userPhoneForm'
+export { UserPasswordForm } from './userPasswordForm'
