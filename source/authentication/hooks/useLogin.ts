00de5889a350791fa51128e42a194978cfbdb60a
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-05-27 22:28:16 -0300

    Feat - integrated login

--- source/authentication/hooks/useLogin.ts
+++ source/authentication/hooks/useLogin.ts
@@ -1,31 +1,29 @@
 import { USER_KEY } from './../../core/constants/localStorageKeys';
 import { MOCKED_USER_DATA } from 'core/utils';
 import { useState } from 'react';
-import { useLocalStorage, useUser } from "core/hooks"
+import { useLocalStorage, useUser, usePostRequest } from "core/hooks"
 import { User } from 'core/interfaces';
-// import { usePostRequest, useUser } from "core/hooks"
-// import { User } from "core/interfaces"
-// import { UsePostRequestStatus } from 'core/hooks/usePostRequest';
+import { UsePostRequestStatus } from 'core/hooks/usePostRequest';
 
-type Status = 'IDLE' | 'SUCCESS' | 'ERROR'
 
-// interface LoginVariable {
-//   cpf: string,
-//   password: string
-// }
+interface TError {
+  message: string
+}
+interface LoginVariable {
+  cpf: string,
+  password: string
+}
 
-// interface ResponseData extends User { }
+interface ResponseData extends User { }
 
 interface UseLoginState {
   error: string
   isLoading: boolean
-  status: Status
-  // status: UsePostRequestStatus,
+  status: UsePostRequestStatus
 }
 
 interface UseLoginActions {
-  // requestLogin: (cpf: string, password: string) => void
-  requestLogin: (cpf: string, password: string, user?: User) => void
+  requestLogin: (cpf: string, password: string) => void
   requestLogout: () => void
 }
 
@@ -38,62 +36,27 @@ type UseLoginData = [
 export const useLogin = (): UseLoginData => {
   const [, { setUser }] = useUser()
   const [error, setError] = useState<string>()
-  const [isLoading, setLoading] = useState(false)
-  const [status, setStatus] = useState<Status>('IDLE')
   const [, { storeData }] = useLocalStorage<User>()
 
 
+  const { mutate, status, isLoading } = usePostRequest<ResponseData, LoginVariable, TError>('/auth/login', {
+    onSuccess: ({ data }) => {
+      const userData = { ...data, isLogged: true }
+      storeData(USER_KEY, userData)
+      setUser(userData)
+    },
+    onError: ({ response }) => {
+      const { data: { message } } = response
+      setError(message)
+    }
+  })
 
-  /**
-   * router: auth/login
-   * body:
-   *  cpf
-   *  password
-   * 
-   * sucesso: 
-   *  status: ok
-   *    body:
-   *      user
-   *      token
-   * 
-   * error:
-   *  status: 404
-   *    usuario nao encontrado
-   *          _
-   *    login ou senha invalidos
-   * 
-   */
-
-
-  // const { mutate, status, isLoading } = usePostRequest<ResponseData, LoginVariable, string>('/login', {
-  //   onSuccess: ({ data }) => {
-  // const userData = {...user, isLogged: true}
-  // storeData(USER_KEY, userData)
-  //     setUser(userData)
-  //   },
-  //   onError: ({ message }) => {
-  //     setError(message)
-  //   }
-  // })
-
-  const requestLogin = (cpf: string, password: string, user = MOCKED_USER_DATA) => {
-    // mutate({ cpf, password })
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-      if (cpf === '09907658499' && password === '123123') {
-        setStatus('SUCCESS')
-        const userData = { ...user, isLogged: true }
-        storeData(USER_KEY, userData)
-        return setUser(userData)
-      }
-      setStatus('ERROR')
-      setError('invalid credentials')
-    }, 1500)
+  const requestLogin = (cpf: string, password: string) => {
+    mutate({ cpf, password })
   }
 
   const requestLogout = () => {
-    setUser({ ...MOCKED_USER_DATA, isLogged: false })
+    setUser(undefined)
     storeData(USER_KEY, null)
   }
 
